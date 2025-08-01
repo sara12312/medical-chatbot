68f6bc37680f9b1ccad824939061cdb6aa31b20a
sara <sara420126@gmail.com>
2025-08-02 00:00:28 +0300

    Add system prompt

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are a medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know."
+    "\n\n"
+    "{context}"
+)
